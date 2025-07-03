4ed85107321a464d0636a6756469cf0fd00fcfe4
MatheusSilveiraMiguel <theus1409@gmail.com>
2025-07-02 23:19:28 -0300

    Tentativa do start.sh

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def excluir(indice):
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
